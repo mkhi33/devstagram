299166faa6b2dd5d386b93227ee9f2df945ff11c
Amilcar Antonio Rodriguez Velasquez <aarodriguezv@unah.hn>
2022-08-02 08:58:05 -0600

    Registrando usuarios

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -32,6 +32,15 @@ class RegisterController extends Controller
             'password' => Hash::make($request->password),
         ]);
 
+        // Autenticar al usuario
+        // auth()->attempt([
+        //     'email' => $request->email,
+        //     'password' => $request->password
+        // ]);
+
+        // Otra forma de autenticar
+        auth()->attempt($request->only(['email', 'password']));
+
         // redireccionar al usuario
         return redirect()->route('posts.index');
     }
